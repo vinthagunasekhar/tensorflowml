8aac0ab35e64200cff5368a2f97dff2d25217b47
Guna Sekhar Vintha <vinthag@mcmaster.ca>
2024-01-11 12:43:58 -0500

    Created a new project for horse-or-human classification using CNN with Training Validation data set.

---
+++ horse-human.py
@@ -0,0 +1,45 @@
+import urllib.request
+import zipfile
+
+url="https://storage.googleapis.com/learning-datasets/horse-or-human.zip"
+
+file_name="horse-or-human.zip"
+
+training_dir='horse-or-human/training/'
+urllib.request.urlretrieve(url,file_name)
+
+zip_ref= zipfile.ZipFile(file_name, 'r')
+zip_ref.extractall(training_dir)
+zip_ref.close()
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers.legacy import RMSprop
+
+train_datagen= ImageDataGenerator(rescale=1/255)
+
+train_generator= train_datagen.flow_from_directory(
+    training_dir,
+    target_size=(300,300),
+    class_mode='binary'
+)
+
+model= tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16,(3,3), activation='relu',input_shape=(300,300,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32,(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy',
+              optimizer= RMSprop(learning_rate=0.001),
+              metrics=['accuracy'])
+history=model.fit_generator(train_generator, epochs=15)
